788c2aa29b04549df9faea912da94db17349ace8
spide0 <rabiulislamarnab456@gmail.com>
2024-02-28 02:37:25 +0600

    taking input

--- countConsonant.js
+++ countConsonant.js
@@ -2,9 +2,9 @@ function countConsonant(str) {
     //"arnab"
     let count=0;
     let a=["a","e","i","o","u"];
+    let call=prompt("Enter your sentence :")
   //here i is the index, str[i] is the character at that index
     for(let i=0; i<str.length; i++) {
-        // if(str[i]==="a" ||str[i]==="e" ||str[i]==="i" || str[i]==="o" || str[i]==="u" )
         if(!a.includes(str[i]))
         {
          count++;
